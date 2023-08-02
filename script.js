79f0739ab3ee2a7b99d6e90e5bdfbf87c1b023f6
JoeCoroSeam <31105607+joeecoroseam@users.noreply.github.com>
2023-08-02 15:35:12 +0200

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken='pk.eyJ1Ijoiam9lY29yb25lbyIsImEiOiJjbGtqcWFhZXYwMnc2M3BsOWRsanVhOHllIn0.4CsHVAQTJJSiAwXbu7NF1g'
+
+const javaMap = new mapboxgl.Map({
+  
+  container : "map",
+  style : 'mapbox://styles/joecoroneo/clktqgct400tv01pbadfgfc4w',
+  center: [-96.33491033674169, 16.937240855272538],
+  zoom: 3
+  
+})
